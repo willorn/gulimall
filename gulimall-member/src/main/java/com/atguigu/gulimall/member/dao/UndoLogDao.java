6d859b2c50636e59eccd663b3ab3ba798970a48f
willorn <willorn@qq.com>
2021-08-03 19:13:06 +0800

    逆向工程基本跑通，可链接数据库向外部返回请求（补上我的member会员模块）

---
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/dao/UndoLogDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.member.dao;
+
+import com.atguigu.gulimall.member.entity.UndoLogEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 
+ * 
+ * @author willorn
+ * @email 2596066608@qq.com
+ * @date 2021-08-03 17:43:51
+ */
+@Mapper
+public interface UndoLogDao extends BaseMapper<UndoLogEntity> {
+	
+}
