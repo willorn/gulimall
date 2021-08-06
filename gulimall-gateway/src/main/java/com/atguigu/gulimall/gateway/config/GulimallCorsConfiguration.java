c36c29067a18acfe4b7aca9657e4bcd490a5c3b6
willorn <willorn@qq.com>
2021-08-06 10:03:31 +0800

    跨域问题解决，P48，下一步三级列表的分类维护。 springboot版本为2.2.4.RELEASE，版本较高，需要给跨域改为allowedOriginPatterns("*")，允许跨域请求的域名，相当于在服务器给予一个响应。

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -6,24 +6,25 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-/**
- * @author willorn
- */
-@Configuration
+@Configuration // gateway
 public class GulimallCorsConfiguration {
 
-    @Bean
+    @Bean // 添加过滤器
     public CorsWebFilter corsWebFilter(){
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-
+        // 基于url跨域，选择reactive包下的
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        // 跨域配置信息
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-
-        //1、配置跨域
+        // 允许跨域的头
         corsConfiguration.addAllowedHeader("*");
+        // 允许跨域的请求方式
         corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addAllowedOrigin("*");
+        // 允许跨域的请求来源
+        corsConfiguration.addAllowedOriginPattern("*");
+        // 是否允许携带cookie跨域
         corsConfiguration.setAllowCredentials(true);
 
+        // 任意url都要进行跨域配置
         source.registerCorsConfiguration("/**",corsConfiguration);
         return new CorsWebFilter(source);
     }
